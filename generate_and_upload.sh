3bf89010c3c4c938545d44c310146a19feac9f5d
Michael Hansen <mihansen@microsoft.com>
2018-10-10 10:04:26 -0700

    Added additional Syntea argument

--- generate_and_upload.sh
+++ generate_and_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /app/synthea
-./run_synthea -p $NUMBER_OF_PATIENTS
+./run_synthea -p $NUMBER_OF_PATIENTS $ADDITIONAL_SYNTHEA_ARGS
 
 cd /app
 dotnet run /app/synthea/output/fhir $FHIR_SERVER_URL
